5015afd938a7d169f0126ba7a4e8b66c3d57c647
Dayana Cardoso <dayana.automacao@gmail.com>
2025-08-26 18:01:44 +0000

    update simulation parameters in tarefa2.py

--- multi-armed-bandit/agentes/epsilon_greedy.py
+++ multi-armed-bandit/agentes/epsilon_greedy.py
@@ -1,24 +1,26 @@
 from interfaces.agent import Agent
 import numpy as np
 class EpsilonGreedySampleAverageAgent(Agent):
-    def __init__(self, k_arm=10, epsilon=0.1, initial_estimate=0):
+    def __init__(self, k_arm=10, epsilon=0.1, initial_estimate=0.):
         super().__init__(k_arm, initial_estimate)
         self.epsilon = epsilon
-        self.k_arm = k_arm
-        self.q_valor = np.zeros(self.k_arm) #ESTIMATIVA DO VALOR DA AÇÃO
+        self.valor_estimado = np.zeros(self.k_arm) #ESTIMATIVA DO VALOR DA AÇÃO
+        # iniciar com destibuição normal
+        # self.valor_estimado = np.random.normal(0, 1, self.k_arm)
         self.n_tentativas = np.zeros(self.k_arm) #NUMERO DE VEZES QUE A AÇÃO FOI TENTADA
-
+        self.reset()
+        
     def act(self):
         # Escolha da ação sengundo a politica epsilon-gulosa
         if np.random.random() < self.epsilon:
             action = np.random.randint(self.k_arm)
         else:
-            action = np.argmax(self.q_valor)
+            action = np.argmax(self.valor_estimado)
         return action
 
     def reset(self):
-        self.q_valor = np.full(self.k_arm, self.initial_estimate)
-        self.n_tentativas = np.zeros(self.k_arm)
+        self.valor_estimado = np.full(self.k_arm, self.initial)
+        
 
     def update(self, action, reward):
         # Atualização incremental do valor da ação escolhida segundo a media amostral
@@ -30,26 +32,28 @@ class EpsilonGreedySampleAverageAgent(Agent):
         step_size = (1 / self.n_tentativas[action])
 
         # Q[a] ← Q[a] + step_size * (reward - Q[a])
-        self.q_valor[action] += step_size * (reward - self.q_valor[action])
+        self.valor_estimado[action] += step_size * (reward - self.valor_estimado[action])
 
 class EpsilonGreedyConstantStepsizeAgent(Agent):
     def __init__(self, k_arm=10, epsilon=0.1, initial_estimate=0., step_size=0.1):
         super().__init__(k_arm, initial_estimate)
         self.epsilon = epsilon
-        self.k_arm = k_arm
         self.step_size = step_size # passo constante
-        self.q_valor = np.zeros(self.k_arm) #ESTIMATIVA DO VALOR DA AÇÃO
-
+        self.valor_estimado = np.zeros(self.k_arm) #ESTIMATIVA DO VALOR DA AÇÃO
+        self.reset()
         def act(self):
           # Escolha da ação sengundo a politica epsilon-gulosa
           if np.random.random() < self.epsilon:
               action = np.random.randint(self.k_arm)
           else:
-              action = np.argmax(self.q_valor)
+              action = np.argmax(self.valor_estimado)
           return action
 
     def update(self, action, reward):
         # Atualização do valor da ação escolhida segundo a media amostral
         # Q[a] ← Q[a] + step_size * (reward - Q[a])
-        self.q_valor[action] += self.step_size * (reward - self.q_valor[action])
+        self.valor_estimado[action] += self.step_size * (reward - self.valor_estimado[action])
 
+    def reset(self):
+        return super().reset()
+    
